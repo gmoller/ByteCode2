2ca0375c0b87a57724e27697b66abdb58c5e1e2e
gmoller <greg.moller@gmail.com>
2018-04-04 20:33:47 -0700

    Add project files.

---
+++ ByteCode/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteCode
+{
+    public class Class1
+    {
+    }
+}
